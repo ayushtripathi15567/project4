25a790ab547e87b0ff1d5a34a2d3e08d08b25b63
ayushtripathi15567 <tripathiayush175@gmail.com>
2024-05-23 13:59:23 +0530

    first commit

---
+++ src/components/Store.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import CounterReducer from './Counter'
+import { configureStore } from '@reduxjs/toolkit'
+
+export let store = configureStore({
+    reducer:{
+        counter:CounterReducer
+    }
+})
